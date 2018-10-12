b715b62989c8ea6afe7a4c8a259277684c75e51c
jikagq <theo-nec@hotmail.fr>
2018-10-12 18:49:21 +0200

    ajout des fonctions de deplacement avec odometre

--- WifibotWIFI/WifibotWIFI/main.cpp
+++ WifibotWIFI/WifibotWIFI/main.cpp
@@ -8,9 +8,6 @@
 
 WifibotClient robot;
 SensorData sensors_data;
-int IR_SEUIL = 100;
-
-
 
 int SharpLUT[]={150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,103,102,101,100,99,
           96,95,90,88,87,86,85,84,83,82,81,80,79,77,75,74,73,72,71,70,69,68,67,66,65,65,64,64,63,62,61,61,60,60,59,59,58,58,57,57,56,56,56,55,55,55,54,54,54,53,
@@ -18,6 +15,7 @@ int SharpLUT[]={150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,
                   38,38,37,37,37,37,36,36,36,36,35,35,35,35,34,34,34,34,34,34,33,33,33,33,32,32,32,32,32,31,31,31,31,30,30,30,30,30,30,30,
           30,29,29,29,29,29,29,29,28,28,28,28,28,28,27,27,27,27,27,27,27,26,26,26,26,25,25,25,25,25,25,24,24,24,24,24,23,23,23,
           23,23,23,22,22,22,22,22,21,21,21,21,21,21,20,20,20,20,19,19,19,19,19,19,18,18,18,18,18,18,17,17,17,16,16,15,15};
+
 
 void Robot_Avancer(UINT16 vitesse_gauche, UINT16 vitesse_droite)
 {
@@ -45,28 +43,80 @@ void Robot_Tourner_Droite(UINT16 vitesse_gauche, UINT16 vitesse_droite)
 
 void Robot_Arreter(UINT16 vitesse_gauche, UINT16 vitesse_droite)
 {
-	unsigned char flags = 128 + 64 + 32 + 16 + 1;
+	unsigned char flags = 0 + 0 + 0 + 0 + 0;
 	robot.SendCommand(vitesse_gauche, vitesse_droite, flags);
 }
 
-void Wifibot_CapteurIR() 
+
+
+
+
+/*avec tick*/
+void Robot_Avancer_avec_tick(UINT16 vitesse_gauche, UINT16 vitesse_droite, int consigne_tick)
 {
-		printf("IRLeft : %d\n", sensors_data.IRLeft);
-		printf("IRRight : %d\n", sensors_data.IRRight);
+	int val_tick_droite = 0;
+	int val_tick_gauche = 0;
 
-		// Test IR left
-		//if (sensors_data.IRLeft < IR_SEUIL) {
-		//	Robot_Arreter();
-		//}
+	while ((val_tick_droite < consigne_tick) && (val_tick_gauche < consigne_tick)) {
 
-		// Test IR Right
-		//if (sensors_data.IRRight < IR_SEUIL) {
-		//	Robot_Arreter();
-		//}
+		Robot_Avancer(vitesse_gauche, vitesse_droite);
+		robot.GetSensorData(&sensors_data);//aquisition
+		val_tick_droite = sensors_data.OdometryRight;//actualisation
+		val_tick_gauche = sensors_data.OdometryLeft;//actualisation
+	}
+	Robot_Arreter(0, 0);
 }
+
+void Robot_Reculer_avec_tick(UINT16 vitesse_gauche, UINT16 vitesse_droite, int consigne_tick)
+{
+	int val_tick_droite = 0;
+	int val_tick_gauche = 0;
+
+	while ((val_tick_droite < consigne_tick) && (val_tick_gauche < consigne_tick)) {
+
+		Robot_Reculer(vitesse_gauche, vitesse_droite);
+		robot.GetSensorData(&sensors_data);//aquisition
+		val_tick_droite = sensors_data.OdometryRight;//actualisation
+		val_tick_gauche = sensors_data.OdometryLeft;//actualisation
+	}
+	Robot_Arreter(0, 0);
+}
+
+void Robot_Tourner_Gauche_avec_tick(UINT16 vitesse_gauche, UINT16 vitesse_droite, int consigne_tick)
+{
+	int val_tick_droite = 0; //variable du tick mesuré
+	while (val_tick_droite < consigne_tick) {
+
+		Robot_Tourner_Gauche(vitesse_gauche, vitesse_droite);
+		robot.GetSensorData(&sensors_data);//aquisition
+		val_tick_droite = sensors_data.OdometryRight;//actualisation
+	}
+	Robot_Arreter(0, 0);
+}
+
+void Robot_Tourner_Droite_avec_tick(UINT16 vitesse_gauche, UINT16 vitesse_droite, int consigne_tick)
+{
+	int val_tick_gauche = 0; //variable du tick mesuré
+	while (val_tick_gauche < consigne_tick) {
+
+		Robot_Tourner_Droite(vitesse_gauche, vitesse_droite);
+		robot.GetSensorData(&sensors_data);//aquisition
+		val_tick_gauche = sensors_data.OdometryLeft;//actualisation
+	}
+	Robot_Arreter(0, 0);
+}
+
+/*fin avec tick*/
+
+
+
+
 
 void main(void)
 {
+
+	
+
 	/*.........................*/
 	/* Connection to the robot */
 	/*.........................*/
@@ -88,26 +138,21 @@ void main(void)
 	/* Main program */
 	/*..............*/
 	
-	SensorData sensors_data;
+	
 	while(1)
 	{
-		Robot_Avancer(255, 255);
+		unsigned char flags = 128+32+64+16+1;
+		robot.SendCommand(255,255,flags);
+		
 		robot.GetSensorData(&sensors_data);
-		printf("IRLeft : %d\n", sensors_data.IRLeft);
-		printf("IRRight : %d\n", sensors_data.IRRight);
-
-		// Test IR left
-		while (sensors_data.IRLeft > IR_SEUIL || sensors_data.IRRight > IR_SEUIL)
-		{
-			Robot_Arreter(0,0);
-			robot.GetSensorData(&sensors_data);
-			printf("IRLeft : %d\n", sensors_data.IRLeft);
-			printf("IRRight : %d\n", sensors_data.IRRight);
-		}
-		robot.GetSensorData(&sensors_data);
-		//printf("Batterie : %d\n", sensors_data.BatVoltage);
+		printf("Batterie : %d\n", sensors_data.BatVoltage);
 		
 		
-		//Sleep(100);
+		
+		
+		print("test odo droite", sensors_data.OdometryRight);
+		print("test odo gauche", sensors_data.OdometryLeft);
+		
+		Sleep(100);
 	}	
 }
